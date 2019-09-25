109fdc0a7333d0dc3f858e7675a87e97b86d5008
helio junior <lab8467@caelum144-12>
2019-09-25 20:38:16 -0300

    Acrescentada SessaoController and Model da Sessao

---
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -0,0 +1,51 @@
+package br.com.caelum.ingresso.model;
+
+import java.time.LocalTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//@Entity
+public class Sessao {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Filme filme;
+	private Sala sala;
+	
+	
+	
+	public Sessao(LocalTime horario, Filme filme, Sala sala ) {
+		this.filme = filme;
+		this.sala = sala;
+		this.horario = horario;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Filme getFilme() {
+		return filme;
+	}
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+	public Sala getSala() {
+		return sala;
+	}
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+	public LocalTime getHorario() {
+		return horario;
+	}
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+	private LocalTime horario;
+}
