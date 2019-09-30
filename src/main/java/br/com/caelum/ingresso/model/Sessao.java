d60c7b1e99a2cbe2eb6c47931dd76df148d1c550
helio junior <lab8467@caelum144-12>
2019-09-30 20:10:30 -0300

    Ajuste dos preços. Inclusão do campo BigDecimal

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -22,6 +24,8 @@ public class Sessao {
 	@ManyToOne
 	private Sala sala;
 	
+	private BigDecimal preco;
+	
 	/**
 	 * @deprecated hibernate only
 	 */
@@ -32,6 +36,15 @@ public class Sessao {
 		this.filme = filme;
 		this.sala = sala;
 		this.horario = horario;
+		this.preco = sala.getPreco().add(filme.getPreco());
+	}
+	
+	public BigDecimal getPreco() {
+		return preco.setScale(2,RoundingMode.HALF_UP);
+	}
+	
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
 	}
 	
 	public Integer getId() {
